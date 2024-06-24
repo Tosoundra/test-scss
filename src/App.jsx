4134bf55fcac9bcbeda08900d767197fb60c3eb6
Antony Kowalski <beheaded_96@mail.ru>
2024-06-24 17:47:46 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import styles from './styles.module.scss';
+
+function App() {
+	return (
+		<>
+			<div className={styles.div}></div>
+		</>
+	);
+}
+
+export default App;
