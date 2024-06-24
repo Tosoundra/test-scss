0b56fa1c09dae6f81bdb63c47375f356e89f63cc
Antony Kowalski <beheaded_96@mail.ru>
2024-06-24 18:12:50 +0700

    init 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import styles from './styles.module.scss';
+import { Comp } from './components/Comp';
 
 function App() {
 	return (
 		<>
 			<div className={styles.div}></div>
+			<Comp />
 		</>
 	);
 }
---
+++ src/components/Comp.jsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.scss';
+
+export const Comp = (params) => {
+	return <div style={styles.comp}></div>;
+};
